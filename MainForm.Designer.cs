d87b031d6629188af589ec6ca73b498d58aec050
THE834R <g-arga1@hotmail.com>
2014-06-06 18:22:49 -0500

    practica_5

---
+++ MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bel Kiel
+ * Date: 04/05/2014
+ * Time: 06:39 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PRACTICA_5
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label6 = new System.Windows.Forms.Label();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.Responder = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(28, 52);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 40);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Selecciona a integrantes de los paises aliados";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(24, 182);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(150, 36);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Gobernante de Italia durante la segunda guerra mundial";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(24, 95);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Alemania";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(24, 125);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(104, 24);
+			this.checkBox2.TabIndex = 3;
+			this.checkBox2.Text = "Japón";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Location = new System.Drawing.Point(24, 155);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(104, 24);
+			this.checkBox3.TabIndex = 4;
+			this.checkBox3.Text = "Inglaterra";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(24, 221);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 5;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Mussolini";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(24, 251);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 6;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "JFK";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(24, 281);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 24);
+			this.radioButton3.TabIndex = 7;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Hittler";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(24, 13);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Código";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(24, 29);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 9;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(221, 13);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Nombre";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(221, 29);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(349, 20);
+			this.textBox2.TabIndex = 11;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(259, 69);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(266, 23);
+			this.label5.TabIndex = 12;
+			this.label5.Text = "Cantidad de estados de México";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(259, 95);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+									50,
+									0,
+									0,
+									0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+									1,
+									0,
+									0,
+									0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 13;
+			this.numericUpDown1.Value = new decimal(new int[] {
+									1,
+									0,
+									0,
+									0});
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(259, 125);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(227, 23);
+			this.label6.TabIndex = 14;
+			this.label6.Text = "Fecha de la independencia de México";
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(259, 155);
+			this.monthCalendar1.MaxDate = new System.DateTime(2020, 12, 31, 0, 0, 0, 0);
+			this.monthCalendar1.MinDate = new System.DateTime(1790, 1, 1, 0, 0, 0, 0);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 15;
+			// 
+			// Responder
+			// 
+			this.Responder.Location = new System.Drawing.Point(657, 155);
+			this.Responder.Name = "Responder";
+			this.Responder.Size = new System.Drawing.Size(75, 23);
+			this.Responder.TabIndex = 16;
+			this.Responder.Text = "Contestar";
+			this.Responder.UseVisualStyleBackColor = true;
+			this.Responder.Click += new System.EventHandler(this.ResponderClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1087, 344);
+			this.Controls.Add(this.Responder);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "PRACTICA 5";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button Responder;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PRACTICA_5
+{
+
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		void ResponderClick(object sender, EventArgs e)
+		{
+			
+						int Cont=0;
+						
+			
+			if(this.checkBox3.Checked)
+				{
+				 Cont = Cont +1;
+				 }
+
+			  if(this.radioButton1.Checked)
+			  	{
+				 Cont = Cont +1;
+				}
+			
+			if(this.numericUpDown1.Text == "31")
+			{
+				 Cont = Cont +1;
+			}
+			
+			if(this.monthCalendar1.SelectionStart.Date.ToShortDateString() == "16/09/1810")
+			{
+				 Cont = Cont +1;		
+			}
+			
+						
+						
+
+
+			 System.Windows.Forms.MessageBox.Show("Las respuestas correctas fueron "+ Cont); 
+		}
+		
+		
+		
+		
+	}
+}
